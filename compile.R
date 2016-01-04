bcc2035e98d00f871327ff5408969e1ef905020d
Aydin Demircioglu <aydin.demircioglu@ini.rub.de>
2016-01-04 09:06:30 +0100

    up

--- compile.R
+++ compile.R
@@ -1,9 +1,16 @@
 #!/usr/bin/Rscript  --vanilla 
 
-	library(methods)
-	library(devtools)
-	compileAttributes ()
-	load_all (".")
-	build_vignettes(".")
-	document(".")
-	
+# work around
+    system2(command="./cleanup", args="")
+    
+    library(methods)
+    library(devtools)
+    library(Rcpp)
+    compileAttributes ()
+    load_all (".")
+    build_vignettes(".")
+    document(".")
+    
+# work around
+    system2(command="R", args="CMD build .")
+    system2(command="R", args="CMD check --as-cran ./LiblineaR.ACF_1.94-2.tar.gz")
